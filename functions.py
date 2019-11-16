3d141aa8627d62eec9794d37399b567d5b2c085f
Raul Alzate <raulest50@gmail.com>
2019-11-15 22:55:57 -0500

    se creo una interfaz de usuario personalizada con una aplicacion apra encriptacion y desencriptacion

--- functions.py
+++
@@ -1,6 +0,0 @@
-from cryptography.fernet import Fernet
-
-
-
-def GenerateRandomKey():
-    return Fernet.generate_key()
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import kivy
 kivy.require('1.9.0')
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.event import EventDispatcher # para manejar eventos o listeners de los widgets y eventos temporizados
+
+from cryptography.fernet import Fernet
 
 
 """ this is our custom root widget"""
@@ -16,52 +18,51 @@ class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text='Nombre de Usuario', font_size=40))
-        self.username = TextInput(multiline=False, font_size=40)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='Contraseña',font_size=40))
-        self.password = TextInput(multiline=False, font_size=40)
-        self.add_widget(self.password)
-        self.b1 = Button(text='evento 1', font_size=40)
-        self.add_widget(self.b1)
-        self.b2 = Button(text='evento 2', font_size=40)
-        self.add_widget(self.b2)
+        self.row_default_height = 100
+        self.row_force_default = True
+        self.Button_randkey = Button(text='Generar', font_size=30)
+        self.TextIn_randkey = TextInput(multiline=False, font_size=30, size_hint_x=None, width=700)
+        self.Button_backUp = Button(text='BackUp', font_size=30)
+        self.TextIn_route = TextInput(multiline=False, font_size=30, size_hint_x=None, width=700)
 
-        self.b1.bind(on_press=self.eventoX)
+        self.add_widget(self.Button_randkey)
+        self.add_widget(self.TextIn_randkey)
+        self.add_widget(self.Button_backUp)
+        self.add_widget(self.TextIn_route)
 
-    """
-    funcion que limpia el campo de texto 
-    """
-    def eventoX(self, obj):
-        self.username.text=''
+        self.Button_randkey.bind(on_press=self.ActionButton_randkey)
+        self.Button_backUp.bind(on_press=self.ActionButton_backUp)
 
+    def ActionButton_randkey(self, obj):
+        self.TextIn_randkey._set_text(Fernet.generate_key())
 
+    def ActionButton_backUp(self, obj):
+        key = self.TextIn_randkey.text
+        ruta = self.TextIn_route.text
+        file = open(ruta, 'rb')
+        contenido = file.read()
+        #encoded = contenido.encode()
+        f = Fernet(key)
+        encri = f.encrypt(contenido)
+        encriFile = open('encri.txt', 'wb')
+        encriFile.write(encri)
+        encriFile.close()
 
+        decriFile = open('dencri.txt', 'wb')
+        decriFile.write(f.decrypt(encri))
+        decriFile.close()
+        print(f.decrypt(encri))
 
-"""
-clase para manejar eventos personalizados
-"""
-class MyEventDispatcher(EventDispatcher):
-
-    def __init__(self):
-        self.register_event_type('evento1')
-        self.register_event_type('evento2')
-        super(MyEventDispatcher, self).__init__(**kwargs)
-
-    def evento1(self, *args):
-        print('nothing')
-
-    def evento2(self, *args):
-        print('also nothing')
 
 """
 se requiere que la clase base de tu apicacion herede de la clase App de kivy
 """
+
+
 class MyApp(App):
 
     def build(self):
         return LoginScreen()
-
 
 
 if __name__ == '__main__':
