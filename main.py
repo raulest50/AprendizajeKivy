f9673e6f638d5b33934c320edf884ad3717bbdee
Raul Alzate <raulest50@gmail.com>
2019-11-16 11:47:55 -0500

    Se avanzo en el grado de complejidad usando kivy. Cuando las interfacez graficas se hacen mas complicadas es mas conveniente tener la presentacion de la interfaz grafica de la logica de la aplicacion. para lograr esto se usa un archivo de extension .kv. Por convencion este se debe llamar como la clase que hereda de App pero en minuscula y sin App. Ejemplo:
    si en python la clase que hereda de App se es
     class MyApp(App):
    
     entonces el archivo de la GUI sera my.kv.
    
     all seguir esta convencion el todo se enlaza automaticamente. En este commit se tiene una version mas sofisticada de la intefaz grafica en el archivo encriptar.kv

--- main.py
+++ main.py
@@ -11,59 +11,36 @@ from kivy.event import EventDispatcher # para manejar eventos o listeners de los
 from cryptography.fernet import Fernet
 
 
-""" this is our custom root widget"""
-class LoginScreen(GridLayout):
-
-    # method override to add widgets and define behavior
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.row_default_height = 100
-        self.row_force_default = True
-        self.Button_randkey = Button(text='Generar', font_size=30)
-        self.TextIn_randkey = TextInput(multiline=False, font_size=30, size_hint_x=None, width=700)
-        self.Button_backUp = Button(text='BackUp', font_size=30)
-        self.TextIn_route = TextInput(multiline=False, font_size=30, size_hint_x=None, width=700)
-
-        self.add_widget(self.Button_randkey)
-        self.add_widget(self.TextIn_randkey)
-        self.add_widget(self.Button_backUp)
-        self.add_widget(self.TextIn_route)
-
-        self.Button_randkey.bind(on_press=self.ActionButton_randkey)
-        self.Button_backUp.bind(on_press=self.ActionButton_backUp)
-
-    def ActionButton_randkey(self, obj):
-        self.TextIn_randkey._set_text(Fernet.generate_key())
-
-    def ActionButton_backUp(self, obj):
-        key = self.TextIn_randkey.text
-        ruta = self.TextIn_route.text
-        file = open(ruta, 'rb')
-        contenido = file.read()
-        #encoded = contenido.encode()
-        f = Fernet(key)
-        encri = f.encrypt(contenido)
-        encriFile = open('encri.txt', 'wb')
-        encriFile.write(encri)
-        encriFile.close()
-
-        decriFile = open('dencri.txt', 'wb')
-        decriFile.write(f.decrypt(encri))
-        decriFile.close()
-        print(f.decrypt(encri))
 
 
-"""
-se requiere que la clase base de tu apicacion herede de la clase App de kivy
-"""
+def ActionButton_randkey(self, obj):
+    self.TextIn_randkey._set_text(Fernet.generate_key())
 
+def ActionButton_backUp(self, obj):
+    key = self.TextIn_randkey.text
+    ruta = self.TextIn_route.text
+    file = open(ruta, 'rb')
+    contenido = file.read()
+    #encoded = contenido.encode()
+    f = Fernet(key)
+    encri = f.encrypt(contenido)
+    encriFile = open('encri.txt', 'wb')
+    encriFile.write(encri)
+    encriFile.close()
 
-class MyApp(App):
+    decriFile = open('dencri.txt', 'wb')
+    decriFile.write(f.decrypt(encri))
+    decriFile.close()
+    print(f.decrypt(encri))
+
+class EncriptarApp(App):
 
     def build(self):
-        return LoginScreen()
+        pass
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    EncriptarApp().run()
+
+
+#C:\Users\Raul Alzate\PycharmProjects\venvKivy\share\kivy-examples\demo\kivycatalog
